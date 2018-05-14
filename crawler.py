5b360a740bc14329acc5e49e04ba944c726a0461
Daniel <daniel.torbacka@gmail.com>
2018-05-14 17:59:21 +0200

    made it multithreaded

--- crawler.py
+++ crawler.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from html_parser import parser
 from database import adDao
 
@@ -5,11 +7,18 @@ from database import adDao
 def main():
     # print("html_parser")
     ads = parser.parseListPage()
-    print(len(ads))
-    for ad in ads:
-        ad = parser.parseAdPage(ad, ad['Url'])
-        print(ad)
-        adDao.insertAd(ad)
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        executor.map(addAds, ads)
+
+
+def addOldAds(i):
+    ad = parser.parseAdPage(dict(), "Lista/Details/?aid=" + i)
+    adDao.insertAd(ad)
+
+def addAds(ad):
+    ad = parser.parseAdPage(ad, ad['Url'])
+    print(ad)
+    adDao.insertAd(ad)
 
 
 if __name__ == "__main__":
