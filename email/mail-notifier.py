681fabc985ab295253cdc7fea0cab69583a3607d
Daniel Törbacka <daniel.torbacka@gmail.com>
2018-06-11 08:10:58 +0200

    add mail notification

---
+++ email/mail-notifier.py
@@ -0,0 +1,32 @@
+import datetime
+import os
+import requests
+
+from database import adDao
+
+mailgun_url = os.getenv("MAILGUN_URL")
+api_key = os.getenv("API_KEY")
+mail = os.getenv("MAIL")
+
+
+def notify():
+    ads = get_ads()
+
+
+def get_ads():
+    min_date = datetime.datetime(2014, 1, 1, 0, 0, 0)
+    return adDao.retrieve({
+        "$and:": [
+            {"Queue": {"$elemMatch": {"$lt": min_date}}},
+            {"Type": {"$nin": ["Student"]}}
+        ]
+    })
+
+
+def mail(recipient, message):
+    requests.post(mailgun_url, auth=("api", api_key), data={
+        'from': 'bf-notifier@torbacka.se',
+        'to': recipient,
+        'subject': 'Intressa lägenheter från (bostad.stockholm.se)',
+        'text': message
+    })
